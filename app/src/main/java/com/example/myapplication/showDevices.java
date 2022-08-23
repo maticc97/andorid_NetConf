d1895ad3bba41f95f216084bb1e18ce8235f57df
Matic Lulik <matic.lulik@gmail.com>
2022-08-23 22:53:11 +0200

    devices list VIEW

--- app/src/main/java/com/example/myapplication/showDevices.java
+++ app/src/main/java/com/example/myapplication/showDevices.java
@@ -2,29 +2,146 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class showDevices extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class showDevices extends AppCompatActivity {
+    //get extras from another activity
+    Bundle extras;
+
+    //get value by key
+    String selectedCustomer;
+    private String TAG = showDevices.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> devicesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //get extras from another activity
-        Bundle extras = getIntent().getExtras();
-
-        //get value by key
-        String selectedCustomer = extras.getString("customer");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_devices);
+
+        lv = (ListView) findViewById(R.id.list_devices);
+        devicesList = new ArrayList<>();
+
+        extras= getIntent().getExtras();
+        selectedCustomer= extras.getString("customer");
+        TextView cust_label = (TextView)findViewById(R.id.customer_label);
+        cust_label.setText(selectedCustomer);
 
         Toast t = Toast.makeText(getApplicationContext(),
                 "Pressed Item " + selectedCustomer,
                 Toast.LENGTH_LONG);
-        t.show();
+
+
+
+        new getDevices().execute();
+
     }
+    private class getDevices extends AsyncTask<Void, Void, Void> {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(showDevices.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://192.168.1.143:5000/api/v1/customers/"+selectedCustomer+"/devices/";
+            String jsonStr = sh.makeServiceCall(url);
+
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+
+                try {
+                    // Getting JSON Array node
+                    JSONArray devices = new JSONArray(jsonStr);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < devices.length(); i++) {
+                        JSONObject c = devices.getJSONObject(i);
+                        String id = c.getString("_id");
+                        String hostname = c.getString("hostname");
+                        String type = c.getString("type");
+                        String ip_address = c.getString("ip_address");
+                        String added_by = c.getString("added_by");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> device = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        device.put("_id", id);
+                        device.put("hostname", hostname);
+                        device.put("type", type);
+                        device.put("ip_address", ip_address);
+                        device.put("added_by", added_by);
+                        Log.d("a", "a"+device);
+
+                        // adding contact to contact list
+                        devicesList.add(device);
+
+                    }
+
+                    Log.d("aa", "device list");
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(showDevices.this, devicesList,
+                    R.layout.list_devices, new String[]{"hostname", "type", "ip_address", "added_by"},
+                    new int[]{R.id.hostname, R.id.type_text, R.id.ip_addr_text, R.id.added_by_text});
+            lv.setAdapter(adapter);
+
+        }
+
+    }
+
 
 
 }
